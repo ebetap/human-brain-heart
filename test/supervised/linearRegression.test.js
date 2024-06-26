e6ef851bdefee845e6da08f9eddefcbd637defb5
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:19:17 +0700

    Update linearRegression.test.js

--- test/supervised/linearRegression.test.js
+++ test/supervised/linearRegression.test.js
@@ -1,1 +1,12 @@
+// test/supervised/linearRegression.test.js
+import { LinearRegression } from '../../src/supervised/linearRegression.js';
+import { assert } from 'chai';
 
+const X = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+const y = [3, 5, 7, 11, 15];
+
+const model = new LinearRegression();
+model.fit(X, y);
+const predictions = model.predict(X);
+
+console.log('Predictions:', predictions);
