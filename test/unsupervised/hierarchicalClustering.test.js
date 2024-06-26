1a2b69c01824422ba79242095374eed67cba7c26
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:24:22 +0700

    Update hierarchicalClustering.test.js

--- test/unsupervised/hierarchicalClustering.test.js
+++ test/unsupervised/hierarchicalClustering.test.js
@@ -1,1 +1,15 @@
+// test/unsupervised/hierarchicalClustering.test.js
+import { HierarchicalClustering } from '../../src/unsupervised/hierarchicalClustering.js';
+import { assert } from 'chai';
 
+const X = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+
+const model = new HierarchicalClustering();
+model.fit(X);
+const clusters = model.predict(X);
+
+console.log('Dendrogram:', clusters);
+
+assert.isObject(clusters);
+assert.property(clusters, 'index');
+assert.property(clusters, 'children');
