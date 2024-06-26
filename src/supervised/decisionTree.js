2b1280dc3a9a5801d9e447edc7268e1e7f8c0d71
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 14:30:04 +0700

    Update decisionTree.js

--- src/supervised/decisionTree.js
+++ src/supervised/decisionTree.js
@@ -1,1 +1,19 @@
+// src/supervised/decisionTree.js
+import { DecisionTreeClassifier } from 'ml-cart';
 
+class DecisionTree {
+    constructor() {
+        this.model = null;
+    }
+
+    fit(X, y) {
+        this.model = new DecisionTreeClassifier();
+        this.model.train(X, y);
+    }
+
+    predict(X) {
+        return this.model.predict(X);
+    }
+}
+
+export { DecisionTree };
