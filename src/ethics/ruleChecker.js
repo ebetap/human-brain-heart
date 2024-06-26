628a64150682075c31ca060a5e2003628b0b8029
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 21:36:41 +0700

    Update ruleChecker.js

--- src/ethics/ruleChecker.js
+++ src/ethics/ruleChecker.js
@@ -1,1 +1,25 @@
+// src/ethics/ruleChecker.js
 
+class Rule {
+    constructor(description, checkFunction) {
+        this.description = description;
+        this.check = checkFunction;
+    }
+}
+
+const noHarmRule = new Rule(
+    'The robot should not harm humans.',
+    (action) => action.type !== 'harm' || action.target !== 'human'
+);
+
+const privacyRule = new Rule(
+    'The robot should respect human privacy.',
+    (action) => action.type !== 'access_private_data'
+);
+
+const fairnessRule = new Rule(
+    'The robot should act fairly.',
+    (action) => action.type !== 'discriminate'
+);
+
+export { Rule, noHarmRule, privacyRule, fairnessRule };
