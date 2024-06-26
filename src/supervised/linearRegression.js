6da7b82119b5ce17491adf9e96b4809e4de5d338
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 14:19:28 +0700

    Update linearRegression.js

--- src/supervised/linearRegression.js
+++ src/supervised/linearRegression.js
@@ -1,1 +1,23 @@
+// src/supervised/linearRegression.js
+import { Matrix } from 'ml-matrix';
 
+class LinearRegression {
+    constructor() {
+        this.coefficients = null;
+    }
+
+    fit(X, y) {
+        const XMatrix = new Matrix(X);
+        const yMatrix = Matrix.columnVector(y);
+        const XTX = XMatrix.transpose().mmul(XMatrix);
+        const XTy = XMatrix.transpose().mmul(yMatrix);
+        this.coefficients = XTX.inverse().mmul(XTy);
+    }
+
+    predict(X) {
+        const XMatrix = new Matrix(X);
+        return XMatrix.mmul(this.coefficients).to1DArray();
+    }
+}
+
+export { LinearRegression };
