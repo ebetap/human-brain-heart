074c90250034526554bf3e30d14077071b01fc69
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:16:19 +0700

    Update mathUtils.js

--- src/utils/mathUtils.js
+++ src/utils/mathUtils.js
@@ -1,1 +1,25 @@
+// src/utils/mathUtils.js
+import { Matrix } from 'ml-matrix';
 
+const math = {
+    transpose: function (matrix) {
+        return new Matrix(matrix).transpose();
+    },
+    multiply: function (a, b) {
+        return new Matrix(a).mmul(new Matrix(b));
+    },
+    inv: function (matrix) {
+        return new Matrix(matrix).inverse();
+    },
+    dot: function (a, b) {
+        return new Matrix(a).mmul(new Matrix(b)).to1DArray();
+    },
+    subtract: function (a, b) {
+        return new Matrix(a).subtract(new Matrix(b)).to1DArray();
+    },
+    add: function (a, b) {
+        return new Matrix(a).add(new Matrix(b)).to1DArray();
+    },
+};
+
+export { math };
