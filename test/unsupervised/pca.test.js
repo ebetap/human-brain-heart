2cbbd4e8e742004f07bd02c5d240034e56883932
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:24:45 +0700

    Update pca.test.js

--- test/unsupervised/pca.test.js
+++ test/unsupervised/pca.test.js
@@ -1,1 +1,14 @@
+// test/unsupervised/pca.test.js
+import { PCA } from '../../src/unsupervised/pca.js';
+import { assert } from 'chai';
 
+const X = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+
+const model = new PCA();
+model.fit(X);
+const transformed = model.transform(X);
+
+console.log('Transformed Data:', transformed);
+
+assert.isArray(transformed);
+assert.lengthOf(transformed, X.length);
