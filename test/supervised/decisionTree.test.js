22e368573e5ab9821b29d17f0c5855af6024ba7e
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:20:45 +0700

    Update decisionTree.test.js

--- test/supervised/decisionTree.test.js
+++ test/supervised/decisionTree.test.js
@@ -1,1 +1,14 @@
+// test/supervised/decisionTree.test.js
+import { DecisionTree } from '../../src/supervised/decisionTree.js';
+import { assert } from 'chai';
 
+const X = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+const y = [0, 0, 1, 1, 1];
+
+const model = new DecisionTree();
+model.fit(X, y);
+const predictions = model.predict(X);
+
+console.log('Predictions:', predictions);
+
+assert.deepEqual(predictions, y);
