9297d63c4196088041f58353cec0aa23b80aac18
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 21:36:08 +0700

    Update ethicsFramework.js

--- src/ethics/ethicsFramework.js
+++ src/ethics/ethicsFramework.js
@@ -1,1 +1,22 @@
+// src/ethics/ethicsFramework.js
 
+class EthicsFramework {
+    constructor() {
+        this.rules = [];
+    }
+
+    addRule(rule) {
+        this.rules.push(rule);
+    }
+
+    checkAction(action) {
+        for (const rule of this.rules) {
+            if (!rule.check(action)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+export { EthicsFramework };
