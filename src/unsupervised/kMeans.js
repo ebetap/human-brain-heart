12cc83b6669e5e7751f316f8010a607bd9472aba
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 14:34:29 +0700

    Update kMeans.js

--- src/unsupervised/kMeans.js
+++ src/unsupervised/kMeans.js
@@ -1,1 +1,19 @@
+// src/unsupervised/kMeans.js
+import { KMeans as MLKMeans } from 'ml-kmeans';
 
+class KMeans {
+    constructor(k) {
+        this.k = k;
+        this.model = null;
+    }
+
+    fit(X) {
+        this.model = new MLKMeans(X, this.k);
+    }
+
+    predict(X) {
+        return this.model.predict(X);
+    }
+}
+
+export { KMeans };
