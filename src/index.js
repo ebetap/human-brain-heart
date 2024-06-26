a355559f74787e1b3b8e4af6001d9c0ba2c1831d
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:17:37 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+// src/index.js
+import { LinearRegression } from './supervised/linearRegression.js';
+import { LogisticRegression } from './supervised/logisticRegression.js';
+import { DecisionTree } from './supervised/decisionTree.js';
+import { SVM } from './supervised/svm.js';
+import { KMeans } from './unsupervised/kMeans.js';
+import { HierarchicalClustering } from './unsupervised/hierarchicalClustering.js';
+import { PCA } from './unsupervised/pca.js';
+import { math } from './utils/mathUtils.js';
+import { dataProcessing } from './utils/dataProcessing.js';
 
+export {
+    LinearRegression,
+    LogisticRegression,
+    DecisionTree,
+    SVM,
+    KMeans,
+    HierarchicalClustering,
+    PCA,
+    math,
+    dataProcessing,
+};
