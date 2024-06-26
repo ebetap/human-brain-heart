9afe96855314614b48a2ca68e0a9a4939db457b6
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 14:31:00 +0700

    Update svm.js

--- src/supervised/svm.js
+++ src/supervised/svm.js
@@ -1,1 +1,18 @@
+// src/supervised/svm.js
+import SVM from 'ml-svm';
 
+class SVM {
+    constructor() {
+        this.model = new SVM();
+    }
+
+    fit(X, y) {
+        this.model.train(X, y);
+    }
+
+    predict(X) {
+        return this.model.predict(X);
+    }
+}
+
+export { SVM };
