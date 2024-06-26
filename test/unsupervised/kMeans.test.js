3501e7c86b0ebdc9af2d67f1905fa93738e360a8
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:24:01 +0700

    Update kMeans.test.js

--- test/unsupervised/kMeans.test.js
+++ test/unsupervised/kMeans.test.js
@@ -1,1 +1,15 @@
+// test/unsupervised/kMeans.test.js
+import { KMeans } from '../../src/unsupervised/kMeans.js';
+import { assert } from 'chai';
 
+const X = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+
+const model = new KMeans(2);
+model.fit(X);
+const predictions = model.predict(X);
+
+console.log('Cluster Assignments:', predictions);
+
+// We won't assert exact cluster assignments because they can vary due to the random initialization.
+assert.isArray(predictions);
+assert.lengthOf(predictions, X.length);
