55a655e8305b60ba53e592ffaa4205f586efb770
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:26:37 +0700

    Update dataProcessing.test.js

--- test/utils/dataProcessing.test.js
+++ test/utils/dataProcessing.test.js
@@ -1,1 +1,16 @@
+// test/utils/dataProcessing.test.js
+import { dataProcessing } from '../../src/utils/dataProcessing.js';
+import { assert } from 'chai';
 
+const data = [[1, 2], [2, 3], [3, 4], [5, 6], [7, 8]];
+
+const normalizedData = dataProcessing.normalize(data);
+const standardizedData = dataProcessing.standardize(data);
+
+console.log('Normalized Data:', normalizedData);
+console.log('Standardized Data:', standardizedData);
+
+assert.isArray(normalizedData);
+assert.lengthOf(normalizedData, data.length);
+assert.isArray(standardizedData);
+assert.lengthOf(standardizedData, data.length);
