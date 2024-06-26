f76c7250936cacc5b97735c5203c8221cd6d3581
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 19:13:10 +0700

    Update pca.js

--- src/unsupervised/pca.js
+++ src/unsupervised/pca.js
@@ -1,1 +1,18 @@
+// src/unsupervised/pca.js
+import { PCA as MLPCA } from 'ml-pca';
 
+class PCA {
+    constructor() {
+        this.model = null;
+    }
+
+    fit(X) {
+        this.model = new MLPCA(X);
+    }
+
+    transform(X) {
+        return this.model.predict(X);
+    }
+}
+
+export { PCA };
