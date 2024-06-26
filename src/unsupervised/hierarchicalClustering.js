216ce8a19aa887f210297223b101b490a440088f
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-26 14:35:04 +0700

    Update hierarchicalClustering.js

--- src/unsupervised/hierarchicalClustering.js
+++ src/unsupervised/hierarchicalClustering.js
@@ -1,1 +1,21 @@
 
+// src/unsupervised/hierarchicalClustering.js
+import { agnes } from 'ml-hclust';
+
+class HierarchicalClustering {
+    constructor() {
+        this.clusters = null;
+    }
+
+    fit(X) {
+        this.clusters = agnes(X);
+    }
+
+    predict(X) {
+        // Hierarchical clustering typically doesn't have a predict method.
+        // Instead, the fit method is used to create a dendrogram which can be cut at a specific level.
+        return this.clusters;
+    }
+}
+
+export { HierarchicalClustering };
